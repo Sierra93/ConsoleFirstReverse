478f394fbca1096b955fa203d99b7afc3779e78b
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2019-06-17 00:07:40 +0300

    Доработка программы

--- ConsoleFirstReverse/Program.cs
+++ ConsoleFirstReverse/Program.cs
@@ -5,8 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleFirstReverse {
+    // Суть программы: 
+    // На входе вводится строка. На выходе получаем эту строку в обратно порядке.
     class Program {
-        static void Main(string[] args) {
+        public static string FirstReverse(string str) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = str.Length - 1; i >= 0; i--) {
+                sb.Append(str[i]);
+            }
+            return sb.ToString();
+        }
+        static void Main() {
+            Console.WriteLine(FirstReverse(Console.ReadLine()));
+            Console.ReadKey();
         }
     }
 }
